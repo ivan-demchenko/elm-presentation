23d95ca2a8e85a70642412afee27980ff001f850
Ivan Demchenko <1846484+raqystyle@users.noreply.github.com>
2023-09-25 15:28:14 +0000

    chore: simple program

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,43 @@
 module Main exposing (..)
 
+import Browser exposing (..)
 import Html exposing (..)
 
 
-main : Html msg
+main : Program () Model Msg
 main =
-    div []
-        [ text "Hello"
-        ]
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { counter : Int
+    }
+
+
+type Msg
+    = Noop
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model 0, Cmd.none )
+
+
+view : Model -> Html Msg
+view _ =
+    div [] [ text "hello" ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
